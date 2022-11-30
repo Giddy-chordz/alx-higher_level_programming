c2d91f595702b760b28b341371f794a1c114f654
Oyegbami Gideon <gideonakinpelumi@gmail.com>
2022-11-29 19:45:22 -0800

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,11 @@
-//user definition//
 #!/usr/bin/python3
-//Import random numbers//
+#Import random numbers#
 import random
 number = random.randint(-10000, 10000)
-//write a program that will detect print the the last digit of a random number//
-//codition1: If greater than 5//
-//condition2: If the number is 0//
-//condition3: If the number is less than 6 and not 0//
+#write a program that will detect print the the last digit of a random number#
+#codition1: If greater than 5
+#condition2: If the number is 0#
+#condition3: If the number is less than 6 and not 0#
 print(f"Last digit of {number} is {number}", end="\n")
 if (number > 5):
     print("and is greater than 5", end="\n")
